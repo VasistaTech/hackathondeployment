cb9a74be96369e9016593932d6a243f506968f1c
VasistaTech <vasistatech19@gmail.com>
2024-12-09 22:40:27 +0530

    Update or create file via GitHub API

--- lib/constants/skills_item.dart
+++ lib/constants/skills_item.dart
@@ -1,27 +1,17 @@
 const List<Map> platformItems = [ 
  { 
 
-"img" :"assets/Unix_1.png",
+"img" :"assets/Hive_finally.png",
 
-"title": "Unix",
- }, { 
-
-"img" :"assets/DataBricks_123.png",
-
-"title": "DataBricks",
+"title": "Hive",
  },
 ];
 
  const List<Map> skillItem = [ 
  { 
 
-"img" :"assets/Hive_finally.png",
+"img" :"assets/DataBricks_123.png",
 
-"title": "Hive",
- }, { 
-
-"img" :"assets/pypi-final-val_2.png",
-
-"title": "Pypi",
+"title": "DataBricks",
  },
 ]; 
