92a981137f520d8eb3fd90d678d4af98ab98f26a
VasistaTech <vasistatech19@gmail.com>
2024-12-09 22:40:25 +0530

    Update or create file via GitHub API

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,22 +2,22 @@ import 'dart:ui';
 
         class CustomColor{
 
-        static const Color scaffloldBg = Color(0xff0e1b2b);
+        static const Color scaffloldBg = Color(0xff32475c);
 
-        static const Color bgLight1 = Color(0xff1e232b);
+        static const Color bgLight1 = Color(0xff075e5e);
 
-        static const Color bgLight2 = Color(0xff34363b );
+        static const Color bgLight2 = Color(0xff0b6d56 );
 
-        static const Color textFieldBg = Color(0xffdfe2e4);
+        static const Color textFieldBg = Color(0xffeae4e4);
 
-        static const Color hintDark = Color(0xff716d6d);
+        static const Color hintDark = Color(0xff585353);
 
-        static const Color yellowsecondary = Color(0xffffb300);
+        static const Color yellowsecondary = Color(0xffffaa00);
 
-        static const Color yellowPrimary = Color(0xfff3bd0f);
+        static const Color yellowPrimary = Color(0xffff7d00);
 
-        static const Color whitePrimary = Color(0xffffffff);
+        static const Color whitePrimary = Color(0xfff5f0f0);
 
-        static const Color whilteSecondary = Color(0xffffffff);
+        static const Color whilteSecondary = Color(0xfff5eded);
  
         }
